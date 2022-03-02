be8b715c0892e43915a351df7263c29b9ee4fa0b
it-kia <49620136+it-kia@users.noreply.github.com>
2022-03-02 03:19:39 +0300

    Часть ДЗ по 3му уроку

--- src/main/java/ru/gb/Lesson_003/Sample_01.java
+++ src/main/java/ru/gb/Lesson_003/Sample_01.java
@@ -1,83 +1,186 @@
 package ru.gb.Lesson_003;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Sample_01 {
 
     static Scanner scanner = new Scanner(System.in);
 
-    static void checkNumber(){
-        System.out.println("Подсчет суммы нечетных положительных чисел");
-        System.out.println("==========================================");
-        System.out.println("Вводите целые числа (0 - завершение ввода)");
-
-        int sum = 0;
-        int counter = 0;
-        boolean isNumber;
-        int number = 0;
-
-        do{
-            counter++;
-            System.out.print("Введите число: ");
-            isNumber = scanner.hasNextInt();
-            if (isNumber){
-                number = scanner.nextInt();
-                scanner.nextLine();
-                //if (!isEven(number) && number > 0)
-                //    sum += number; // sum = sum + number;
-            }
-            else {
-                System.out.println("Вы ввели некорректное число.\nПожалуйста, повторите поытку ввода.");
-                scanner.nextLine();
-            }
-
-        }
-        while (!(isNumber && number == 0));
-
-        System.out.printf("Сумма нечетных положительных чисел: %d;\nКол-во попыток ввода %d\n",
-                sum, counter);
-        System.out.println("Завершение работы подпрограммы");
-    }
-
     public static void main(String[] args) {
 
+        // Запускаем меню
+        main_menu();
+
+    }
+
+    /**
+     * Метод для работы главного меню
+     */
+    static void main_menu(){
         boolean f = true;
+        int no;
 
         while (f)
         {
-            System.out.println("1 - Task1");
-            System.out.println("2 - Task2");
-            System.out.println("Введите номер задачи (0 - выход из программы): ");
+            System.out.println("==============================================");
+            System.out.println("Выберите номер игры, в которую хотите поиграть");
+            System.out.println("==============================================");
+            System.out.println("1 - Угадай число");
+            System.out.println("2 - Угадай слово");
+            System.out.println("0 - Выход из программы");
+            System.out.println("==============================================");
+            System.out.print("Введите номер: ");
 
-            if (scanner.hasNextInt()){
-                int no = scanner.nextInt();
+            if (scanner.hasNextInt()) {
+                no = scanner.nextInt();
                 scanner.nextLine();
 
-                switch (no){
+                switch (no) {
                     case 0:
                         System.out.println("Завершение работы приложения.");
                         scanner.close();
                         f = false;
                         break;
-                    //return;
                     case 1:
-                        checkNumber();
-                        break;
-                    case 2:
-                        //task2();
-                        break;
-                    default:
-                        System.out.println("Номер задачи не найден.\nПожалуйста, повторите попытку ввода.");
-                }
+                        //guess_number();
+                        System.out.println("=================================================");
+                        System.out.println("        Выберите уровень сложности игры");
+                        System.out.println("=================================================");
+                        System.out.println("1 - Простая (числа от 0 до 9, дается 3 попытки)");
+                        System.out.println("2 - Средняя (числа от 0 до 50, дается 5 попыток)");
+                        System.out.println("3 - Сложная (числа от 0 до 100, дается 7 попыток)");
+                        System.out.println("0 - Возврат в главное меню");
+                        System.out.println("=================================================");
+                        System.out.print("Введите номер: ");
 
+
+                        if (scanner.hasNextInt()) {
+                            no = scanner.nextInt();
+                            scanner.nextLine();
+
+                            switch (no) {
+                                case 0:
+                                    f = true;
+                                    break;
+
+                                case 1:
+                                    guess_number(no);
+                                    break;
+                                case 2:
+                                    guess_number(no);
+                                    break;
+                                case 3:
+                                    guess_number(no);
+                                    break;
+                                default:
+                                    System.out.println("Уровень сложности не найден.\nПожалуйста, повторите попытку ввода.");
+                            }
+                        }
+                            break;
+                            case 2:
+                                guess_word();
+                                break;
+                            default:
+                                System.out.println("Номер игры не найден.\nПожалуйста, повторите попытку ввода.");
+
+                }
             }
             else
             {
-                System.out.println("Вы ввели некорректное число.\nПожалуйста, повторите поытку ввода.");
+                System.out.println("Вы ввели некорректное число.\nПожалуйста, повторите попытку ввода.");
                 scanner.nextLine();
             }
         }
-        //System.out.println("Завершение работы приложения.");
+    }
+
+    /**
+     * Метод игры "Угадай число"
+     */
+    static void guess_number (int level){
+
+        Random random = new Random();
+
+        //Создаем переменные
+        int maxNumber = 0; //Максимальное значение загаданного числа
+        int hiddenNum = 0; //Загаданное число
+        int maxCount = 0; //Максимальное количество попыток отгадать
+        int nCount = 0; //Счетчик попыток
+        int enteredNum = -1; //Введенное число
+        boolean isNumber;
+        boolean a = true;
+
+        //Делаем настройки для выбранного уровня
+        if (level == 1){
+            maxNumber = 10;
+            hiddenNum = random.nextInt(maxNumber);
+            maxCount = 3;
+        }
+        if (level == 2){
+            maxNumber = 50;
+            hiddenNum = random.nextInt(maxNumber);
+            maxCount = 5;
+        }
+        if (level == 3){
+            maxNumber = 100;
+            hiddenNum = random.nextInt(maxNumber);
+            maxCount = 7;
+        }
+
+
+        System.out.println("=====================================");
+        System.out.println("Угадайте с " + maxCount + " попыток число от 0 до " + --maxNumber);
+        System.out.println("=====================================");
+
+        // System.out.println("hiddenNum = [" + hiddenNum + "]");
+
+            while (a) {
+
+                //Увеличиваем счетчик
+                nCount++;
+
+                System.out.print("Введите число: ");
+
+                isNumber = scanner.hasNextInt();
+                if (isNumber) {
+                    enteredNum = scanner.nextInt();
+                    scanner.nextLine();
+
+                    if (enteredNum > hiddenNum) {
+                        if (nCount < maxCount) {
+                            System.out.printf("Загаданное число меньше, попробуйте угадать еще!\nОставшееся число попыток: %d\n", maxCount - nCount);
+                        } else {
+                            System.out.println("Вы не отгадали и попытки закончились!");
+                            a=false;
+                        }
+                    }
+                    if (enteredNum < hiddenNum) {
+                        if (nCount < maxCount) {
+                            System.out.printf("Загаданное число больше, попробуйте угадать еще!\nОставшееся число попыток: %d\n", maxCount - nCount);
+                        } else {
+                            System.out.println("Вы не отгадали и попытки закончились!");
+                            a=false;
+                        }
+                    }
+                    if (enteredNum == hiddenNum) {
+                        System.out.println("Вы отгадали!");
+                        a=false;
+                    }
+
+                } else {
+                    System.out.println("Вы ввели некорректное число.\nПожалуйста, повторите попытку ввода.");
+                    scanner.nextLine();
+                }
+
+            }
+
+    }
+
+    /**
+     * Метод игры "Угадай слово"
+     */
+    static void guess_word (){
+
     }
 
 }
